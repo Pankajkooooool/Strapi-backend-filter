8439ceed0071638a0b194a700831d94c46127f68
Panakj Kumar <pankajkooooool6@gmail.com>
2022-12-22 23:53:39 +0530

    Yaml files

--- config/server.js
+++ config/server.js
@@ -1,7 +1,14 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  url: env("RENDER_EXTERNAL_URL"),
+  dirs: {
+    public: "/data/public"
   },
 });
